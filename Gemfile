e5dc63d460bd194d1a6f0f96b51011b22162265d
Cong <thanhcong.uet97@gmail.com>
2018-10-03 23:02:25 +0700

    jwt

--- Gemfile
+++ Gemfile
@@ -3,7 +3,9 @@ git_source(:github){|repo| "https://github.com/#{repo}.git"}
 
 ruby "2.5.1"
 
+gem "bcrypt", "~> 3.1.7"
 gem "bootsnap", ">= 1.1.0", require: false
+gem "jwt"
 gem "pg", ">= 0.18", "< 2.0"
 gem "puma", "~> 3.11"
 gem "rails", "~> 5.2.1"
--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -1,7 +1,17 @@
 module ExceptionHandler
   extend ActiveSupport::Concern
 
+  class AuthenticationError < StandardError; end
+  class MissingToken < StandardError; end
+  class InvalidToken < StandardError; end
+
   included do
+    rescue_from ActiveRecord::RecordInvalid, with: :four_twenty_two
+    rescue_from ExceptionHandler::AuthenticationError,
+      with: :unauthorized_request
+    rescue_from ExceptionHandler::MissingToken, with: :four_twenty_two
+    rescue_from ExceptionHandler::InvalidToken, with: :four_twenty_two
+
     rescue_from ActiveRecord::RecordNotFound do |e|
       json_response({message: e.message}, :not_found)
     end
@@ -9,5 +19,15 @@ module ExceptionHandler
     rescue_from ActiveRecord::RecordInvalid do |e|
       json_response({message: e.message}, :unprocessable_entity)
     end
+  end
+
+  private
+
+  def four_twenty_two exception
+    json_response({message: exception.message}, :unprocessable_entity)
+  end
+
+  def unauthorized_request exception
+    json_response({message: exception.message}, :unauthorized)
   end
 end
