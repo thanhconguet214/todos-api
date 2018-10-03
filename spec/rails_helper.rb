7a9c68650f3bd225e5fdb852ae8aa1838c0f0e0c
Cong <thanhcong.uet97@gmail.com>
2018-10-03 21:13:01 +0700

    models

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,3 +1,10 @@
+require "spec_helper"
+ENV["RAILS_ENV"] ||= "test"
+require File.expand_path("../../config/environment", __FILE__)
+# Prevent database truncation if the environment is production
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+require "rspec/rails"
+
 require "database_cleaner"
 
 Shoulda::Matchers.configure do |config|
