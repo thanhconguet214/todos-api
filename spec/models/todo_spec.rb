b6edaa85372bd3801bc9a183d151814faeed427d
Cong <thanhcong.uet97@gmail.com>
2018-10-03 23:02:25 +0700

    authenticate_controller

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Todo, type: :model do
   context "association" do
     it{is_expected.to have_many(:items).dependent :destroy}
   end
-
+  
   context "validation" do
     it{is_expected.to validate_presence_of :title}
     it{is_expected.to validate_presence_of :created_by}
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ require File.expand_path("../../config/environment", __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require "rspec/rails"
 require "database_cleaner"
+
 Dir[Rails.root.join('spec/support/**/*.rb')].each {|f| require f}
 
 Shoulda::Matchers.configure do |config|
